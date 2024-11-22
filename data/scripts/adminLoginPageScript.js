c764bf7af7658b9b81de793d06fb61ff82345b29
pig-game <133531182+pig-game@users.noreply.github.com>
2024-11-22 17:39:21 +0000

    Update adminLoginPageScript.js

--- data/scripts/adminLoginPageScript.js
+++ data/scripts/adminLoginPageScript.js
@@ -1,18 +1,28 @@
-var data = {'VBurden': 'PG426',
-            'TGreen': 'ZK618',
-            'DKennedy': 'JB393'};
-var username = 0; var password = 0; var text = document.getElementById('iscorrect');
+var data = {
+    'VBurden': 'PG426',
+    'TGreen': 'ZK618',
+    'DKennedy': 'JB393'
+};
+
+var username = 0; 
+var password = 0; 
+var text = document.getElementById('iscorrect');
+
 function login() {
-  var username = document.getElementById('username'); var password = document.getElementById('password');
-  if (username in data) {
-    if (data[username] == password) {
-      text.innerHTML = 'Correct. Logging you on...')
-      text.style = 'color: green;';
-      window.location.href = 'https://www.piggame.co.uk/admin/login/loginservice?username=' + username + '&password=' + password;
-    }
-  } else {
-    text.innerHTML = 'Incorrect username or password.';
-    text.style = 'color:red;';
-  }
-}
+    username = document.getElementById('username');
+    password = document.getElementById('password');
     
+    if (username in data) {
+        if (data[username] == password) {
+            text.innerHTML = 'Correct. Logging you on...';
+            text.style.color = 'green';
+            window.location.href = 'https://www.piggame.co.uk/admin/login/loginservice?username=' + username + '&password=' + password;
+        } else {
+            text.innerHTML = 'Incorrect username or password.';
+            text.style.color = 'red';
+        }
+    } else {
+        text.innerHTML = 'Incorrect username or password.';
+        text.style.color = 'red';
+    }
+}
