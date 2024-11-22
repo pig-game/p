ae5cc5db749e026c41828339b1159398839baea5
pig-game <133531182+pig-game@users.noreply.github.com>
2024-11-22 15:54:17 +0000

    Create loadAdmin.js

---
+++ data/scripts/loadAdmin.js
@@ -0,0 +1,7 @@
+//create variables
+//var textarea = document.getElementById('textcenter'); var hasLoggedInBefore = localStorage.getItem('lqowgegretdyiqnwbeerftoyrqe'); 
+var nextUrl = 0;
+nextUrl = 'https://www.piggame.co.uk/admin/login';
+
+//Change the URL
+window.location.href = nextUrl;
