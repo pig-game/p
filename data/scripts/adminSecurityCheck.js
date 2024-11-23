10c1dedb1000befd400a462be770d0ca5fc4f334
pig-game <133531182+pig-game@users.noreply.github.com>
2024-11-23 20:19:03 +0000

    Create adminSecurityCheck.js

---
+++ data/scripts/adminSecurityCheck.js
@@ -0,0 +1,12 @@
+//hack prevention
+var ca = sessionStorage.getItem('currentAccount'); var referrer = document.referrer;
+if (referrer == '') {
+  window.location.replace('https://www.piggame.co.uk/admin/login?source=loginsericewrongurl');
+} else if (referrer {
+  if (ca == null) {
+    //continue
+  } else {
+    window.location.replace('https://www.piggame.co.uk/admin/login?source=loginsericeusernameinsessionstorage');
+  }
+}
+console.info('Hack checks did not detect any hacks.');
