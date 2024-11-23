f82325a1a41e4f70bc8591e3827a3f0e5de5c7b2
pig-game <133531182+pig-game@users.noreply.github.com>
2024-11-23 08:18:51 +0000

    Update adminLoginPageScript.js

--- data/scripts/adminLoginPageScript.js
+++ data/scripts/adminLoginPageScript.js
@@ -9,14 +9,14 @@ var password = 0;
 var text = document.getElementById('iscorrect');
 
 function login() {
-    username = document.getElementById('username');
-    password = document.getElementById('password');
+    username = document.getElementById('username').value;
+    password = document.getElementById('password').value;
     
     if (username in data) {
         if (data[username] == password) {
             text.innerHTML = 'Correct. Logging you on...';
             text.style.color = 'green';
-            window.location.href = 'https://www.piggame.co.uk/admin/login/loginservice?username=' + username + '&password=' + password;
+            window.location.href = 'https://www.piggame.co.uk/admin/login/loginservice.html?username=' + username + '&password=' + password;
         } else {
             text.innerHTML = 'Incorrect username or password.';
             text.style.color = 'red';
