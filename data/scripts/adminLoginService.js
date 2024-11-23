7956757621526e6ff0cbcb447ccced518e9a19bb
pig-game <133531182+pig-game@users.noreply.github.com>
2024-11-23 17:09:40 +0000

    Update adminLoginService.js

--- data/scripts/adminLoginService.js
+++ data/scripts/adminLoginService.js
@@ -7,11 +7,11 @@ const urlParams = new URLSearchParams(window.location.search); const urlusername
 document.write("Hey, " + urlusername + "! I'm still coding this part of the Admin site. Sorry.");
 var ca = sessionStorage.getItem('currentAccount'); var referrer = document.referrer;
 if (referrer == '') {
-  window.location.href = 'https://www.piggame.co.uk/admin/login?source=loginsericewrongurl';
+  window.location.replace('https://www.piggame.co.uk/admin/login?source=loginsericewrongurl');
 } else {
   if (ca == null) {
     sessionStorage.setItem('currentAccount', urlusername);
   } else {
-    window.location.href = 'https://www.piggame.co.uk/admin/login?source=loginsericeusernameinsessionstorage';
+    window.location.replace('https://www.piggame.co.uk/admin/login?source=loginsericeusernameinsessionstorage');
   }
 }
