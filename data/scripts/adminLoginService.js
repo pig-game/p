79e1e7b46df1019469631cdc5f938f885522c4c5
pig-game <133531182+pig-game@users.noreply.github.com>
2024-11-23 12:11:16 +0000

    Create adminLoginService.js

---
+++ data/scripts/adminLoginService.js
@@ -0,0 +1,7 @@
+/*
+ϟAdmin login serviceϟ
+*/
+
+//ϟcreate variablesϟ
+const urlParams = new URLSearchParams(window.location.search); const URLusername = urlParams.get('username'); const URLpassword = urlParams.get('password');
+document.write("Hey, " + username + "! I'm still coding this part of the Admin site. Sorry.");
