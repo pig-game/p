06ef8d3e3d2381f2d346449f1fe0c65675af6b6e
pig-game <133531182+pig-game@users.noreply.github.com>
2024-11-23 20:22:05 +0000

    Update adminLoginService.js

--- data/scripts/adminLoginService.js
+++ data/scripts/adminLoginService.js
@@ -4,19 +4,10 @@
 
 //ϟcreate variablesϟ
 const urlParams = new URLSearchParams(window.location.search); const urlusername = urlParams.get('username'); const urlpassword = urlParams.get('password');
-document.write("Hey, " + urlusername + "! I'm still coding this part of the Admin site. Sorry.");
+var z = Math.floor(Math.random() * 1000000000000);
+document.write("Session ID: " + z);
+sessionStorage.setItem('session-id', z);
 
-//hack prevention
-var ca = sessionStorage.getItem('currentAccount'); var referrer = document.referrer;
-if (referrer == '') {
-  window.location.replace('https://www.piggame.co.uk/admin/login?source=loginsericewrongurl');
-} else if (referrer {
-  if (ca == null) {
-    sessionStorage.setItem('currentAccount', urlusername);
-  } else {
-    window.location.replace('https://www.piggame.co.uk/admin/login?source=loginsericeusernameinsessionstorage');
-  }
-}
 
 //Head to admin home screen
 window.location.replace('https://www.piggame.co.uk/admin/dashboard/home');
