ce14819c95a65b47f119a39edd07cdc85408d411
pig-game <133531182+pig-game@users.noreply.github.com>
2024-11-30 08:11:43 +0000

    Update adminSecurityCheck.js

--- data/scripts/adminSecurityCheck.js
+++ data/scripts/adminSecurityCheck.js
@@ -1,5 +1,5 @@
 //hack prevention
-console.info('Hack prevention checks started...');
+console.info('ggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg');
 var ca = sessionStorage.getItem('currentAccount'); var referrer = document.referrer;
 if (referrer == '') {
   window.location.replace('https://www.piggame.co.uk/admin/login?source=loginsericewrongurl');
